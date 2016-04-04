31fdbe6df417f3811c27aa05b46b9b3bda4599bb
lqtdevs <lqt.dev@gmail.com>
2016-04-04 09:08:19 +0700

    xcxcxcxc

---
+++ baitap/bt1.java
@@ -0,0 +1,16 @@
+//import java.util.*;
+import java.util.Scanner;
+
+class bt1{
+
+	public static void main(String[] args){
+		System.out.println("Nhap vao 1 chuoi va xuat chuoi do ra:");
+		Scanner nhap =new Scanner(System.in);
+		System.out.println("Nhap 1 chuoi vao:");
+		String chuoi = nhap.nextLine();
+		System.out.println("Chuoi vua nhap la:"+chuoi);
+
+	}
+
+
+}
